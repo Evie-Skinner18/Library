d361c0b59c6b6f70db4dadf1542647da39f68344
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-01-08 18:45:05 +0000

    Started linking the app to a Postgres DB

--- Library.Data/LibraryContext.cs
+++ Library.Data/LibraryContext.cs
@@ -6,10 +6,14 @@ namespace Library.Data
 {
     public class LibraryContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=my_host;Database=Library;Username=my_user;Password=my_pw");
+
         public LibraryContext(DbContextOptions options) : base(options)
-        {    
+        {
         }
 
-        public DbSet<Customer> Customers { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,9 @@ namespace Library
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEntityFrameworkNpgsql();    
+            services.AddDbContext<LibraryContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("LibraryContext")));
         }
 
         // MIDDLEWARE
