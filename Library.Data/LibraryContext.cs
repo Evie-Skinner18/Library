931b192b8b16da40cfbaaf3713628de594a38e5d
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-01-08 18:25:47 +0000

    Moved Models dir to Library.Data class lib project. Updated using statements. Added POCO for Customer to Models

---
+++ Library.Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {    
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Library.Data/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LibraryData.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
