4eb35c2e1678ff0ec13a1ecc9e0b9b26c876e89b
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-01-09 18:29:34 +0000

    Added magazine class

---
+++ Library.Data/Models/LibraryItems/Magazine.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LibraryData.Models.Common;
+
+namespace LibraryData.Models.LibraryItems
+{
+    public class Magazine : LibraryItem, IBorrowable
+    {
+        [Required]
+        public string Topic { get; set; }
+
+        public bool HasCollectibleInside { get; set; }
+
+        public string Borrow()
+        {
+            return "You have borrowed a magazine";
+        }
+
+        public string Return()
+        {
+            return "Thank you for returning that magazine!";
+        }
+
+        public string Review(int numberOfStars)
+        {
+            return $"Thank you for giving this magazine {numberOfStars} stars!";
+        }
+    }
+
+
+}
--- Library.Data/Models/LibraryItems/Video.cs
+++ Library.Data/Models/LibraryItems/Video.cs
@@ -28,6 +28,5 @@ namespace LibraryData.Models.LibraryItems
         }
     }
 
-    //public class 
 
 }
