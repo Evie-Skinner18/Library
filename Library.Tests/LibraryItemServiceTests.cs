75a960bb9164a7a6229b45aba456e8291681bd0b
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-01-14 16:56:13 +0000

    Added more to Setup method

--- Library.Tests/LibraryItemServiceTests.cs
+++ Library.Tests/LibraryItemServiceTests.cs
@@ -1,14 +1,31 @@
+using System.Collections.Generic;
+using LibraryData.Models.Common;
+using LibraryData.Models.LibraryItems;
 using NUnit.Framework;
 
 namespace Library.Tests
 {
     public class LibraryItemServiceTests
     {
-
+        private IBorrowable _fakeLibraryService;
+        private List<LibraryItem> _libraryItems;
 
         [SetUp]
         public void Setup()
         {
+            _fakeLibraryService = new FakeLibraryItemService();
+            _libraryItems = new List<LibraryItem>()
+            {
+                new Book()
+                {
+                    Title = "On the Road",
+                    Author = "Jack Kerouac",
+                    Id = 1111111,
+                    Year = 1959,
+
+
+                }
+            };
         }
 
         [Test]
